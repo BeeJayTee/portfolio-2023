864073b9fddb9638d322c68d3205f89c29f8cb3c
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2022-11-07 14:32:03 -0500

    something

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,10 @@
 const Navbar = () => {
-  return <div className="navbar">This is a navbar</div>;
+  return (
+    <div className="navbar">
+      This is a navbar
+      <span>this</span>
+    </div>
+  );
 };
 
 export default Navbar;
